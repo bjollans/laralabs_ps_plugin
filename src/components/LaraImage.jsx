365bc5430d9c164ad7ba20d248ef2cb30d33ed9f
Bernard Jollans <jollans94@googlemail.com>
2023-04-04 18:56:09 +0200

    Make images loadable

--- src/components/LaraImage.jsx
+++ src/components/LaraImage.jsx
@@ -1,26 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 export const LaraImage = (props) => {
-    const [img, setImg] = useState();
-    const [selected, setSelected] = useState();
-
-    const fetchImage = async () => {
-      const res = await fetch(props.imageUrl);
-      const imageBlob = await res.blob();
-      const imageObjectURL = URL.createObjectURL(imageBlob);
-      setImg(imageObjectURL);
-    };
-  
-    useEffect(() => {
-      fetchImage();
-    }, []);
-  
     return (
       <>
-        <img 
-        onClick={() => setSelected(!selected)}
-        className={selected? "laraimageselected" : "laraimage"} 
-        width={selected? props.width+10 : props.width} src={img} alt="icons" />
+        <img width={props.width} src={props.img} alt="icons" />
       </>
     );
 } 
--- src/components/LaraImageTable.jsx
+++ src/components/LaraImageTable.jsx
@@ -4,19 +4,59 @@ import "../components/LaraImageTable.css";
 
 export const LaraImageTable = (props) => {
     const [selectedImg, setSelectedImg] = useState();
+    const [cells, setCells] = useState([]);
     const imageBaseURI = 'https://y6uchwsnucplc5fl7riiza6hqy0wpmuf.lambda-url.eu-central-1.on.aws/?id=';
     const columns = 2;
 
     const cellRefs = {};
 
+    const fetchImage = async (imageUrl) => {
+        const res = await fetch(imageUrl);
+        const imageBlob = await res.blob();
+        const imageObjectURL = URL.createObjectURL(imageBlob);
+        return imageObjectURL;
+      };
+
+    const cleanSelected = () => {
+        props.imageIds.forEach((imageId) => cellRefs[imageId].className = "");
+    }
+
+    const downloadIt = async () =>  {
+        const app = require('photoshop').app;
+        const image = await fetch(imageBaseURI + selectedImg);
+        const fs = require('uxp').storage.localFileSystem;
+
+        try {
+          const pluginFolder = await fs.getDataFolder();
+          const img = await image.arrayBuffer();
+          const file = await pluginFolder.createFile("image.png", {overwrite: true});
+          await file.write(img);
+          const currentDocument = app.activeDocument;
+          const newDocument = await app.open(file);
+          if (currentDocument) {
+            await newDocument.activeLayers[0].duplicate(currentDocument);
+            await newDocument.close();
+          }
+          cleanSelected();
+          setSelectedImg(null);
+        } catch (e) {
+          console.log(e);
+        }
+    
+        if (!file) {
+          return;
+        }
+      }
+
     const updateSelected = (imageId, event) => {
         if (imageId == selectedImg) return;
+        cleanSelected();
         if (selectedImg) cellRefs[selectedImg].className = "";
         cellRefs[imageId].className = "selectedImage";
         setSelectedImg(imageId);
     };
 
-    const laraImageCell = (imageId) => {
+    const laraImageCell = async (imageId) => {
         return (
             <td key={imageId} 
                 id={"td-" + imageId} 
@@ -24,14 +64,14 @@ export const LaraImageTable = (props) => {
                 onClick={(event) => updateSelected(imageId, event)}
                 ref={ref => cellRefs[imageId] = ref}
                 >
-                <LaraImage width="200px" imageUrl={imageBaseURI + imageId}/>
+                <LaraImage 
+                    width="200px" 
+                    imageUrl={imageBaseURI + imageId}
+                    img={await fetchImage(imageBaseURI + imageId)}/>
             </td>
         );
     };
-
-    const cells = props.imageIds.map((imageId) => laraImageCell(imageId));
     
-
     const showTable = () => {
         const tableRows = [];
         for (let i = 0; i < cells.length; i = i + columns) {
@@ -40,6 +80,12 @@ export const LaraImageTable = (props) => {
         return tableRows;
     }
     
+    useEffect(() => {
+        Promise.all(props.imageIds
+            .map(async (imageId) => await laraImageCell(imageId)))
+            .then((values) => setCells(values));
+      }, []);
+    
     return (
         <>
             <table>
@@ -47,7 +93,12 @@ export const LaraImageTable = (props) => {
                     {showTable()}
                 </tbody>
             </table>
-            <sp-button style={{marginTop:"15px", marginLeft:"8px"}}>Use</sp-button>
+            <sp-button 
+                style={{marginTop:"15px", marginLeft:"8px"}}
+                onClick={() => require('photoshop').core.executeAsModal(downloadIt)}
+                >
+                    Use
+            </sp-button>
         </>
         
     );
